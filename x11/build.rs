11ff4718f0065b56a6ece048fea2356d4eae69fa
meh <meh@schizofreni.co>
2016-06-17 21:24:11 +0200

    Added support for DPMS extension

--- x11/build.rs
+++ x11/build.rs
@@ -5,6 +5,7 @@
 extern crate pkg_config;
 
 fn main () {
+  if cfg!(feature="dpms") { pkg_config::find_library("xext").unwrap(); }
   if cfg!(feature="glx") { pkg_config::find_library("gl").unwrap(); }
   if cfg!(feature="xcursor") { pkg_config::find_library("xcursor").unwrap(); }
   if cfg!(feature="xf86vmode") { pkg_config::find_library("xxf86vm").unwrap(); }
