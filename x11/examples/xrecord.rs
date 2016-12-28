12aaeecf004316afb552c6087d0f0153161a750c
Daggerbot <daggerbot@localhost.localdomain>
2016-12-28 20:49:34 +0000

    Restructure examples.

--- x11/examples/xrecord.rs
+++ x11/examples/xrecord.rs
@@ -1,5 +1,8 @@
 // Example for X Record Extension
 
+#![cfg_attr(not(feature = "xlib"), allow(dead_code))]
+#![cfg_attr(not(feature = "xlib"), allow(unused_imports))]
+
 extern crate libc;
 extern crate x11;
 
@@ -15,8 +18,14 @@ use std::os::raw::{
 use x11::xlib;
 use x11::xrecord;
 
-static mut event_count:u32 = 0;
+static mut EVENT_COUNT:u32 = 0;
 
+#[cfg(not(all(feature = "xlib", feature = "xrecord")))]
+fn main () {
+    panic!("this example requires `--features 'xlib xrecord'`");
+}
+
+#[cfg(all(feature = "xlib", feature = "xrecord"))]
 fn main () {
     unsafe {
         // Open displays
@@ -29,7 +38,7 @@ fn main () {
         xlib::XSynchronize(dpy_control, 1);
 
         let extension_name = CString::new("RECORD").unwrap();
-        
+
         let extension = xlib::XInitExtension(
             dpy_control,
             extension_name.as_ptr());
@@ -83,14 +92,14 @@ fn main () {
     }
 }
 
-unsafe extern "C" fn record_callback(pointer:*mut i8, raw_data: *mut xrecord::XRecordInterceptData) {
-    event_count += 1;
+unsafe extern "C" fn record_callback(_:*mut i8, raw_data: *mut xrecord::XRecordInterceptData) {
+    EVENT_COUNT += 1;
     let data = &*raw_data;
 
     // Skip server events
     if data.category != xrecord::XRecordFromServer {
         return;
-    }   
+    }
 
     // Cast binary data
     let xdatum = &*(data.data as *mut XRecordDatum);
@@ -105,7 +114,7 @@ unsafe extern "C" fn record_callback(pointer:*mut i8, raw_data: *mut xrecord::XR
     };
 
     println!("Event recieve\t{:?}\tevent.", event_type);
-    
+
     xrecord::XRecordFreeData(raw_data);
 }
 
