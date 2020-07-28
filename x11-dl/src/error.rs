2f6b921dde851c86aa3e2a6838de3efad150c3a8
Erle Pereira <erle@erlepereira.com>
2020-07-28 19:58:01 +1000

    replaced deprecated try with ? operator, & other linter cleanups

--- x11-dl/src/error.rs
+++ x11-dl/src/error.rs
@@ -39,11 +39,12 @@ impl OpenError {
 
 impl Display for OpenError {
   fn fmt (&self, f: &mut Formatter) -> Result<(), ::std::fmt::Error> {
-    try!(f.write_str(self.kind.as_str()));
+    //try!(f.write_str(self.kind.as_str())); TEST Erle July 2020 (on dev branch)
+    f.write_str(self.kind.as_str())?;
     if !self.detail.is_empty() {
-      try!(f.write_str(" ("));
-      try!(f.write_str(self.detail.as_ref()));
-      try!(f.write_str(")"));
+      f.write_str(" (")?;
+      f.write_str(self.detail.as_ref())?;
+      f.write_str(")")?;
     }
     return Ok(());
   }
