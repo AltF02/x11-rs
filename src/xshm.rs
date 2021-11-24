42d0b5c8f819b2de7c2abd71c908c0347933ac72
calculon102 <frank@pixelgerecht.de>
2021-11-24 15:21:08 +0100

    Fix further typos with XShmSegmentInfo

--- src/xshm.rs
+++ src/xshm.rs
@@ -6,12 +6,12 @@ x11_link! { Xext, xext, ["libXext.so.6", "libXext.so"], 10,
     pub fn XShmGetEventBase(_1: *mut Display) -> c_int,
     pub fn XShmQueryVersion(_4: *mut Display, _3: *mut c_int, _2: *mut c_int, _1: *mut Bool) -> Bool,
     pub fn XShmPixmapFormat(_1: *mut Display) -> c_int,
-    pub fn XShmAttach(_2: *mut Display, _1: *mut XshmSegmentInfo) -> Bool,
-    pub fn XShmDetach(_2: *mut Display, _1: *mut XshmSegmentInfo) -> Bool,
+    pub fn XShmAttach(_2: *mut Display, _1: *mut XShmSegmentInfo) -> Bool,
+    pub fn XShmDetach(_2: *mut Display, _1: *mut XShmSegmentInfo) -> Bool,
     pub fn XShmPutImage(_11: *mut Display, _10: Drawable, _9: GC, _8: *mut XImage, _7: c_int, _6: c_int, _5: c_int, _4: c_int, _3: c_uint, _2: c_uint, _1: Bool) -> Bool,
     pub fn XShmGetImage(_6: *mut Display, _5: Drawable, _4: *mut XImage, _3: c_int, _2: c_int, _1: c_uint) -> Bool,
-    pub fn XShmCreateImage(_8: *mut Display, _7: *mut Visual, _6: c_uint, _5: c_int, _4: *mut c_char, _3: *mut XshmSegmentInfo, _2: c_uint, _1: c_uint) -> *mut XImage,
-    pub fn XShmCreatePixMap(_7: *mut Display, _6: Drawable, _5: *mut c_char, _4: *mut XshmSegmentInfo, _3: c_uint, _2: c_uint, _1: c_uint) -> Pixmap,
+    pub fn XShmCreateImage(_8: *mut Display, _7: *mut Visual, _6: c_uint, _5: c_int, _4: *mut c_char, _3: *mut XShmSegmentInfo, _2: c_uint, _1: c_uint) -> *mut XImage,
+    pub fn XShmCreatePixMap(_7: *mut Display, _6: Drawable, _5: *mut c_char, _4: *mut XShmSegmentInfo, _3: c_uint, _2: c_uint, _1: c_uint) -> Pixmap,
 
 variadic:
 globals:
